c7977fac2a54adc75548f865a1a6d420e00ddd00
jadhavabhiee <92208809+jadhavabhiee@users.noreply.github.com>
2022-02-06 08:07:30 +0530

    Add files via upload

---
+++ Q2.js
@@ -0,0 +1,28 @@
+// Check whether the given character is a vowel or not.(Also check for capital characters)
+
+
+var char = prompt("Enter an alphabate");
+
+if(char == "a"){    
+    alert("It is a vowel")
+} else if(char == "e") {
+    alert("It is a vowel")
+} else if(char == "i") {
+    alert("It is a vowel")
+} else if(char == "0") {
+    alert("It is a vowel")
+} else if(char == "u") {
+    alert("It is a vowel")
+} else if(char == "A") {
+    alert("It is a vowel")
+}else if(char == "E") {
+    alert("It is a vowel")
+}else if(char == "I") {
+    alert("It is a vowel")
+}else if(char == "O") {
+    alert("It is a vowel")
+}else if(char == "U") {
+    alert("It is a vowel")
+} else  {
+    alert("It is a consonant")
+}
